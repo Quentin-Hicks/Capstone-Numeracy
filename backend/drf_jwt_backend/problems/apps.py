86396a4b199d3fe678eff100dff013b437f588f9
Quentin <qwhicks2@gmail.com>
2022-04-07 11:20:33 -0400

    Renamed Database + Renamed app in backend

--- backend/drf_jwt_backend/problems/apps.py
+++ backend/drf_jwt_backend/problems/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NumeracyConfig(AppConfig):
+class ProblemsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'numeracy'
+    name = 'problems'
