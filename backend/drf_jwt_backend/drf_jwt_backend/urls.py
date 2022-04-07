b6d5159747144f1fd2353a82f10f4dd1af024db9
Quentin <qwhicks2@gmail.com>
2022-04-07 15:39:34 -0400

    Made Changes To url paths

--- backend/drf_jwt_backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/drf_jwt_backend/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
-    path('api/problems/', include('problems.urls')),
+    path('api/problems', include('problems.urls')),
 ]
