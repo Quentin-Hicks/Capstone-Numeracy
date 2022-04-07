896b7f533b3dca028f1262838b0faf9d81d83ff5
Quentin <qwhicks2@gmail.com>
2022-04-07 11:54:23 -0400

    Created Base Code For views.py File

--- backend/drf_jwt_backend/problems/views.py
+++ backend/drf_jwt_backend/problems/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProblemSerializer
+from .models import Problem
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def problems_list(request):
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def problem_detail(request, pk):
+    if request.method == 'GET':
+        pass
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass
+
