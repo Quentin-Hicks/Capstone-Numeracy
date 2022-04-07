9e0fafc8e3f66c9a6940b77bb981b688b17a3f43
Quentin <qwhicks2@gmail.com>
2022-04-07 19:07:11 -0400

    Imported Components + Fixed Naming Issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,14 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import CollectionPage from "./pages/CollectionPage/CollectionPage"
 import ProblemPage from "./pages/ProblemPage/ProblemPage"
-import SubmmissionPage from "./pages/SubmmissionPage/SubmmissionPage"
+import SubmmissionPage from "./pages/SubmissionPage/SubmissionPage"
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import ProblemCard from "./components/ProblemCard/ProblemCard"
+import ProblemDetails from "./components/ProblemDetails/ProblemDetails"
+import SubmissionPage from "./components/SubmissionForm/SubmissionForm"
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
