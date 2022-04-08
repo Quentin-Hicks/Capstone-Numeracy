2c9e794a44dcea653c989279582665841db48f26
Quentin <qwhicks2@gmail.com>
2022-04-08 12:47:41 -0400

    Created NavBar For Capstone

---
+++ frontend/src/components/CapstoneNavBar/CapstoneNavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import AuthContext from "../../context/AuthContext";
+import "./CapstoneNavBar.css";
+
+const CapstoneNavBar = () => {
+
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default CapstoneNavBar
