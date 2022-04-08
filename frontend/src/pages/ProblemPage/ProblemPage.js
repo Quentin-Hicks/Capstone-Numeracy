d22bb8941330941d3bda1eb312a4b71d3a5ae4a9
Quentin <qwhicks2@gmail.com>
2022-04-07 20:27:53 -0400

    Created Base Code

--- frontend/src/pages/ProblemPage/ProblemPage.js
+++ frontend/src/pages/ProblemPage/ProblemPage.js
@@ -2,3 +2,15 @@ import "./ProblemPage.css"
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+const ProblemPage = () => {
+
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default ProblemPage
--- frontend/src/pages/SubmissionPage/SubmissionPage.js
+++ frontend/src/pages/SubmissionPage/SubmissionPage.js
@@ -2,3 +2,15 @@ import "./SubmissionPage.css"
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+const SubmissionPage = () => {
+
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default SubmissionPage
