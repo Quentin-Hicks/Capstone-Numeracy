7c6f4f413ce87221640088878f7bbb0eb5d7d132
Quentin <qwhicks2@gmail.com>
2022-04-15 11:48:10 -0400

    Base Modal Functionality

--- frontend/src/pages/CapstoneHomePage/CapstoneHomePage.js
+++ frontend/src/pages/CapstoneHomePage/CapstoneHomePage.js
@@ -3,13 +3,25 @@ import { useNavigate, Link } from "react-router-dom";
 import React from "react";
 import { useEffect, useState } from "react";
 import {useParams} from "react-router-dom"
+import Modal from "../../components/Modal/Modal";
 
 const CapstoneHomePage = () => {
 
-    const {something} = useParams()
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <></>
+        <div>
+            <h1>What's your Modal?</h1>
+            <div className='button_wrap'>
+        <h3>Hello Modal</h3>
+        <button onClick={() => setIsOpen(true)}>Show Modal</button>
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+            Modal Disaster
+        </Modal>
+
+        <div className='other'>Other Content</div>
+    </div>
+        </div>
     )
 }
 
