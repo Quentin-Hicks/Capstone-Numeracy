2beb52f9dea49eba7828e648732f91889fe4c95b
Quentin <qwhicks2@gmail.com>
2022-04-28 15:27:57 -0400

    Modified CSS For Navbar Component

--- frontend/src/components/CapstoneNavBar/CapstoneNavBar.jsx
+++ frontend/src/components/CapstoneNavBar/CapstoneNavBar.jsx
@@ -7,28 +7,30 @@ const CapstoneNavBar = () => {
 
     return (
         <div className="navbar">
-          <ul>
-            <li className="brand">
-                <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-                    <p>Home</p>
-                </Link>
-            </li>
-            <li className="brand">
-                <Link to="/collection" style={{ textDecoration: "none", color: "black" }}>
-                    <p>Problem Challenges</p>
-                </Link>
-            </li>
-            <li className="brand">
-                <Link to="/submission" style={{ textDecoration: "none", color: "black" }}>
-                    <p>Submit A Problem</p>
-                </Link>
-            </li>
-            <li className="brand">
-                <Link to="/admin/review" style={{ textDecoration: "none", color: "black" }}>
-                    <p>Review Submissions</p>
-                </Link>
-            </li>
-          </ul>
+          <div className="nav-container">
+            <ul>
+                <li className="brand">
+                    <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+                        <p>Home</p>
+                    </Link>
+                </li>
+                <li className="brand">
+                    <Link to="/collection" style={{ textDecoration: "none", color: "black" }}>
+                        <p>Problem Challenges</p>
+                    </Link>
+                </li>
+                <li className="brand">
+                    <Link to="/submission" style={{ textDecoration: "none", color: "black" }}>
+                        <p>Submit A Problem</p>
+                    </Link>
+                </li>
+                <li className="brand">
+                    <Link to="/admin/review" style={{ textDecoration: "none", color: "black" }}>
+                        <p>Review Submissions</p>
+                    </Link>
+                </li>
+            </ul>
+          </div>
         </div>
     )
 }
